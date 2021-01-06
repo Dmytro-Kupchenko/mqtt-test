e7307726cefcc72ee09742083d0706dc0202777d
Dmytro Kupchenko <dmytro_kupchenko@epam.com>
2021-01-06 11:24:24 +0200

    init

---
+++ src/test/java/com/dmytro/kupchenko/mqttdemo/MqttDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmytro.kupchenko.mqttdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
